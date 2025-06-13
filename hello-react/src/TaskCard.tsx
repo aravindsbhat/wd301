d149a068a57944eae3cc0b684940f0d3a4a91fcc
Aravind S Bhat <aravindsbhat@gmail.com>
2025-06-13 11:56:40 +0000

    Migrated to typescript

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,12 +1,20 @@
 import "./TaskCard.css"
-const TaskCard = (props) => {
+
+interface TaskCardProps {
+  title: string;
+  dueDate?: string;
+  completedAtDate?: string;
+  assignee: string;
+}
+import React from "react";
+const TaskCard: React.FC<TaskCardProps> = (props) => {
     return (
       <div className="TaskItem mb-4 ">
         <h2 className="text-xl font-bold">{props.title}</h2>
         {props.dueDate ? <p> Due on:{props.dueDate}</p> : <p> Completed on:{props.completedAtDate}</p>}
         <p>Assignee: {props.assignee}</p>
       </div>
-    )
+    );
   }
 
   export default TaskCard;

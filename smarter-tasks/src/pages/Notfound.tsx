12027407146e8003373ae03704069879e7c040e1
Aravind S Bhat <aravindsbhat@gmail.com>
2025-06-16 17:51:39 +0000

    L5 debugs

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -7,7 +7,7 @@ const NotFoundPage : React.FC = () => {
         <div className="flex flex-col justify-center items-center h-screen">
             <h1 className="text-4xl font-bold mb-4">404 Not Found</h1>
             <p className="text-lg text-gray-600 mb-4">The page you are looking for does not exist.</p>
-            <button onClick={()=>navigate("/home")} className="backToHomeButton">Go Back to Home</button>
+            <button onClick={()=>navigate("/home")} id="backToHomeButton">Go Back to Home</button>
         </div>
     );
 }
--- smarter-tasks/src/pages/TaskDetailsPage.tsx
+++ smarter-tasks/src/pages/TaskDetailsPage.tsx
@@ -23,9 +23,9 @@ const TaskDetailsPage: React.FC = () => {
     const task = taskState.tasks.find(task=> task.id === Number(id));
 
     return (
-        <div className="bg-white shadow-md rounded-md p-4 m-8">
+        <div className="text-blue-600 shadow-md rounded-md p-4 m-8">
           <div className="flex justify-between items-center mb-4">
-            <h3 className="text-lg font-medium">{task?.title}</h3>
+            <h3 className="text-2xl font-medium">{task?.title}</h3>
           </div>
           <p className="text-gray-600">{task?.desc}</p>
           <p className="text-gray-600">{task? new Date(task.dueDate).toISOString().slice(0,10) : ""}</p>
